56935fd53ef7ab45592e580ea40d0327ab9df7cb
gowhich501 <gowhich501@gmail.com>
2021-07-28 01:51:47 +0900

    打刻する処理の追加

--- python_camera.py
+++ python_camera.py
@@ -1,3 +1,4 @@
+from test import detectFace
 import tkinter as tk
 from tkinter import ttk
 import cv2
@@ -6,13 +7,18 @@ from tkinter import font
 import time
 import boto3
 import requests
+import json
+from selenium import webdriver
 
 BUCKET='inboxgowhich'
 OBJECT_NAME='tempFace.png'
-DELAY=500
+DELAY=100
 BASE_URL = "https://japaneast.api.cognitive.microsoft.com/face/v1.0/"
 SUBSCRIPTION_KEY  = "5fe7753fda7944b9bceaa338a19353d9"
 GROUP_NAME = "dakoku"
+face_cascade = cv2.CascadeClassifier('C:\ProgramData\Anaconda3\pkgs\libopencv-4.5.1-py37ha0199f4_0\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
+jobId=None
+faceId="xxx"
 
 class Application(tk.Frame):
     def __init__(self,master, video_source=0):
@@ -78,38 +84,55 @@ class Application(tk.Frame):
         self.btn_snapshot.configure(width = 15, height = 1, command=self.press_snapshot_button)
         self.btn_snapshot.grid(column=0, row=0, padx=30, pady= 10)
         '''
+        # Stop
+        self.btn_stop = tk.Button( self.frame_btn, text='Stop', font=self.font_btn_big )
+        self.btn_stop.configure( width=15, height=1, command=self.press_stop_button )
+        self.btn_stop.grid( column=0, row=0, padx=30, pady=10 )
 
         # Close
-        self.btn_close = tk.Button( self.frame_btn, text='Close', font=self.font_btn_big )
-        self.btn_close.configure( width=15, height=1, command=self.press_close_button )
-        self.btn_close.grid( column=0, row=0, padx=20, pady=10 )
+        self.btn_close = tk.Button( self.frame_btn, text='Dakoku', font=self.font_btn_big )
+        self.btn_close.configure( width=15, height=1, command=self.press_dakoku_button )
+        self.btn_close.grid( column=1, row=0, padx=20, pady=10 )
 
     def update(self):
+        global jobId
+        global faceId
         #Get a frame from the video source
         _, frame = self.vcap.read()
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
 
+        # OpenCV で顔を検出して四角を描く
+        faces = self.detectFaceLocal(frame)
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
         #self.photo -> Canvas
         self.canvas1.create_image(0,0, image= self.photo, anchor = tk.NW)
 
-        frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #if (len(faces)==0):
+            #print('no face is detected')
+        #else:
+        if (len(faces)!=0):
+            if(faceId == None):
+                frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2.imwrite( OBJECT_NAME,
+                        cv2.cvtColor( frame1, cv2.COLOR_BGR2RGB ) )
+                faceId = self.detectFace()
+                person = self.identifyPerson(faceId["faceId"]) 
 
-        cv2.imwrite( OBJECT_NAME,
-                     cv2.cvtColor( frame1, cv2.COLOR_BGR2RGB ) )
-        faceId = self.detectFace()
-        person = self.identifyPerson(faceId["faceId"]) 
+                if person["candidates"]:
+                    #self.press_stop_button()
+                    personId = person["candidates"][0]["personId"] 
+                    print("personId " + personId) 
+                    personInfo = self.getPersonInfoByPersonId(personId) 
+                    self.dakoku(personInfo)
 
-        if person["candidates"]: #学習データに候補があれば 
-            personId = person["candidates"][0]["personId"] 
-            print("personId " + personId) 
-            personInfo = self.getPersonInfoByPersonId(personId) 
-            print(personInfo["name"]) 
-            print(personInfo["userData"]) 
-        else: 
-            print ("No candidates found")
-
+            #else: 
+            #    print ("No candidates found")
+                
+        jobId = self.master.after(self.delay, self.update)
         """
         s3 = boto3.resource('s3')
 
@@ -126,16 +149,49 @@ class Application(tk.Frame):
 
         print('-----\n{}\n-----'.format(url))
         """
-        self.master.after(self.delay, self.update)
+        
+    def dakoku(self, personInfo):
+        print(personInfo["name"]) 
+        print(personInfo["userData"]) 
 
-    def press_snapshot_button(self):
-        # Get a frame from the video source
-        _, frame = self.vcap.read()
+        userData = json.loads(personInfo["userData"])
+        print(userData["id"])
+        print(userData["pw"])
+        '''
+        # 認識した人の氏名でエゴサするテスト
+        driver = webdriver.Chrome("C:/Goichi/App/chromedriver/chromedriver.exe")
+        driver.get("https://www.google.co.jp/")
+        search = driver.find_element_by_name('q')
+        search.send_keys(personInfo["name"])
+        search.submit()
+        '''
+        # 勤次郎の打刻を実行
+        browser = webdriver.Chrome("C:/Goichi/App/chromedriver/chromedriver.exe")
+        browser.get('https://ini5a.kinjirou-asp.jp/TDAD1702/KinjirouWeb/login.aspx')
+        browser.find_element_by_id('uAspLogin_TxtUser').send_keys("KiA1702009")
+        browser.find_element_by_id('uAspLogin_TxtPass').send_keys("sj9FRP6j")
+        browser.find_element_by_id('uAspLogin_BtnLogin').click()
+        browser.find_element_by_id('TxtUser').send_keys(userData["id"])
+        browser.find_element_by_id('TxtPass').send_keys(userData["pw"])
+        browser.find_element_by_id('BtnLogin').click()
+        browser.get('https://ini5a.kinjirou-asp.jp/TDAD1702/KinjirouWeb/Kinjirou/sinsei/kwsfrk/kwsfrkinp.aspx?Atype=11')
+        #browser.find_element_by_id('Kwsdak1_TxtStartTime1').send_keys("9:00")
+        browser.find_element_by_id('Kwsdak1_TxtEndTime1').send_keys("18:00")
+        browser.find_element_by_id('TxtRiyu').send_keys("顔認識打刻のテストです")
+        #browser.find_element_by_id('BtnInp').click()
+        #alert = browser.switch_to_alert()
+        #alert.accept()
+        #browser.close()
+        #browser.quit()
 
-        frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    def press_dakoku_button(self):
+        global faceId
+        faceId = None
 
-        cv2.imwrite( "frame-" + time.strftime( "%Y-%d-%m-%H-%M-%S" ) + ".jpg",
-                     cv2.cvtColor( frame1, cv2.COLOR_BGR2RGB ) )
+    def press_stop_button(self):
+        global jobId
+        print(jobId)
+        self.master.after_cancel(jobId)
 
     def press_close_button(self):
         self.master.destroy()
@@ -201,6 +257,13 @@ class Application(tk.Frame):
         )
         print(r.json())
         return r.json()
+    
+    def detectFaceLocal(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+        return faces
+
+
 
 def main():
     root = tk.Tk()
